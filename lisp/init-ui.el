c825519494b168187d96e46244c1204ba22d5bee
antonio-petrillo <antonio.petrillo4@studenti.unina.it>
2023-09-15 15:35:52 +0200

    add ef-themes and relative numbers

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,7 +1,12 @@
 ;;; init-ui.el --- Description -*- lexical-binding: t; -*-
 
-(use-package cyberpunk-theme
-  :init (load-theme 'cyberpunk t))
+(use-package cyberpunk-theme)
+
+(use-package ef-themes
+  :init (load-theme 'ef-frost t))
+
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
 
 (provide 'init-ui)
 ;;; init-ui.el ends here
